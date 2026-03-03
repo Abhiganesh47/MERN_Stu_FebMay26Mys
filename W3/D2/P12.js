f0e2a7835fa3f9f5d43f116eeb0ee66d7256ad70
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-03 15:25:04 +0530

    Higher order function

---
+++ W3/D2/P12.js
@@ -0,0 +1,12 @@
+// Higher order function
+// a function which takes another function as parameter or a function returnss another function
+
+function createMultiplier(factor){
+    return function(number){
+        return number*factor;
+    }
+}
+let double = createMultiplier(2);
+console.log("double(10): ",double(10));
+let triple = createMultiplier(3);
+console.log("triplet(30): ",triple(30));
