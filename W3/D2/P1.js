2f2066c06949888d5436f1f779c12c6eaae67daa
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-03 12:33:42 +0530

    arithmetic operators  in JS

---
+++ W3/D2/P1.js
@@ -0,0 +1,15 @@
+//arithmetic operators
+console.log("arithmetic operator");
+const a=10 ,b= 3;
+let c=0;
+console.log("c=",c);
+console.log("a+b: ",a+b);
+console.log("a-b: ",a-b);
+console.log("a*b: ",a*b);
+console.log("a/b: ",a/b);
+console.log("a%b: ",a%b); //modulus
+console.log("a**b: ",a**b); //Exponential
+// console.log("a++: ",a++); //post increment
+// console.log("++a: ",++a); //pre increment
+// console.log("a--: ",a--); //post decrement
+// console.log("--a: ",--a); //pre decrement
