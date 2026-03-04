7f044e1b6070e0f1312bfc0f224946fde1850a35
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-04 12:43:33 +0530

    Objects basics

---
+++ W3/D3/P5.js
@@ -0,0 +1,9 @@
+// Basics of Objects
+const person = {
+    name:"Abhi",
+    age:"21",
+    isStudent: true
+} ;
+console.log("Person: ",person);
+console.log("name:",person.name); //dot notation
+console.log("age:",person["age"]); //bracket notation
