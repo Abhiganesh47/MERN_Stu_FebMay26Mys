7c0d85c9410985b54b388abde886c0bc9eb93d3e
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-04 12:39:40 +0530

    For loop: forward & backward loop and nested loop

---
+++ W3/D3/P1.js
@@ -0,0 +1,16 @@
+// For Loop
+console.log("For Loop examples");
+
+for(let i=0;i<5;i++){ //forward loop
+    console.log("i",i); 
+}
+
+for(let i=4;i>0;i--){ //backward loop
+    console.log("i",i); 
+}
+// Nested For loop
+for(let i=1; i<=3;i++){
+    for(let j=1;j<=2;j++){
+        console.log(`i=${i},j=${j}`);
+    }
+}
