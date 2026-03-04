f0596294a78433cdf37e888e6e509d6c7ead0ad3
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-04 13:39:25 +0530

    added new property ,modifed,deleted, made object constructor

--- W3/D3/P5.js
+++ W3/D3/P5.js
@@ -7,3 +7,20 @@ const person = {
 console.log("Person: ",person);
 console.log("name:",person.name); //dot notation
 console.log("age:",person["age"]); //bracket notation
+
+// Add a new property
+person.city = "Mysore";
+console.log(person);
+// modify
+person.age = "22";
+console.log(person);
+// delete
+delete person.isStudent;
+console.log(person);
+
+// Object constructor
+const car = new Object();
+car.make = "audi";
+car.model = "A4";
+car.year =  2015;
+console.log(car);
