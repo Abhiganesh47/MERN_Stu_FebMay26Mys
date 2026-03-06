0fac4d794124ad4962d24a2c26d36cbf4769276b
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-06 11:42:41 +0530

    changed wholeArray to  IntermediateValue

--- W3/D4/P5.js
+++ W3/D4/P5.js
@@ -8,8 +8,8 @@ let total = nums.reduce((intermediateSUM , current) => intermediateSUM + current
 
 // reduce to Object count by category
 let items =["pen","pencil","pen","eraser"];
-let count = items.reduce((wholeArray,item)=>{
-    wholeArray[item]=(wholeArray[item] || 0)+ 1;
-    return wholeArray;
+let count = items.reduce((IntermediateValue,item)=>{
+    IntermediateValue[item]=(IntermediateValue[item] || 0)+ 1;
+    return IntermediateValue;
 },{});
 console.log("Item count: ",count);
