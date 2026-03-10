85cbc2aa811a75fc9d13a25f221e2dbcbf9086b8
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-10 09:13:33 +0530

    this,    method

---
+++ W3/D5/P11.js
@@ -0,0 +1,16 @@
+// this,    method
+const btn = document.getElementById("compareBtn");
+
+btn.addEventListener("click", function (e) {
+    console.log("Regular function");
+    console.log("This === btn", this === btn);
+    console.log("current target === btn", e.currentTarget === btn);
+    console.log("e.target == btn", e.target === btn);
+});
+
+btn.addEventListener("click", (e) => {
+    console.log("Arrow function");
+    console.log("This === btn", this === btn);
+    console.log("current target === btn", e.currentTarget === btn);
+    console.log("e.target == btn", e.target === btn);
+});
