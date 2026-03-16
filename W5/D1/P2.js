a05989e8ac93b217face2ed5ba32754277b7f09f
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-16 16:32:47 +0530

    Array Distruction

---
+++ W5/D1/P2.js
@@ -0,0 +1,10 @@
+// Array Distruction 
+const items = {
+    item1: "Laptop",
+    item2: "server",
+    item3: "cloudAPI"
+};
+
+for(const[keys,itemName]of Object.entries(items)){
+    console.log((`${keys}:${itemName}`))
+}
