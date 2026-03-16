b4cf6f01419ed656f9be2480e10d6dac59f8181e
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-16 16:44:04 +0530

    Array destruction

--- W5/D1/P2.js
+++ W5/D1/P2.js
@@ -1,9 +1,10 @@
-// Array Distruction 
+// Array destruction
 const items = {
     item1: "Laptop",
     item2: "server",
     item3: "cloudAPI"
 };
+
 
 for(const[keys,itemName]of Object.entries(items)){
     console.log((`${keys}:${itemName}`))
