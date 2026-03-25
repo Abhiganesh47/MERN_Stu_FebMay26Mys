217af63f87ceb311d30c6245ddaa2faa8ab5ac24
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-25 11:16:32 +0530

    Append to a File

--- W6/D3/P2.js
+++ W6/D3/P2.js
@@ -8,11 +8,20 @@ const filePath = path.join(__dirname,"sync-note.txt");
 // Syntax for function usage in module
 // moduleName.functionName()
 
-fs.writeFileSync(filePath,"This file was written using writeFileSync().\nSynchronous operation block execution");
+// fs.writeFileSync(filePath,"This file was written using writeFileSync().\nSynchronous operation block execution");
+// Append to a File
+fs.appendFile(filePath,"New text added using fs.appendFile.",
+function(error){
+    if(error){
+        console.log("Append error: ",error.message);
+        return;
+    }
+    console.log("File content appended");
+});
 
 console.log("File writen synchronously.");
 
 const content = fs.readFileSync(filePath,"utf-8");
 
 console.log("File read synchronously");
-console.log(content)
+console.log("File content: ",content);
