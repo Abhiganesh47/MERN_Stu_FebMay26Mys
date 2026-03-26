fba1c846eaaf796d66eb1ebd62639be924e7d7ae
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-26 12:32:03 +0530

    Creating a simple http server

---
+++ W6/D4/P2.js
@@ -0,0 +1,25 @@
+// Creating a simple http server
+
+const http = require("http");
+
+// createServer(): creates a HTTP server instance 
+// Accepts a callback with two important objects : 
+// 1. req: incoming request details 
+// 2. res: outgoing response control 
+
+const server = http.createServer(function(red,res){
+    // writeHead() sets the response status code and headers 
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    // end() sends the response body and closes the response
+    res.end("Hello from NodeJs HTTP server.");
+});
+// listen() binds the server to a port and starts accepting requests 
+server.listen(3000,function(){
+    console.log("Server is running at http://localhost:3000");
+});
+
+// Port numbers:
+// 0 - 1023: System ports
+// 1024 - 49151: "Registered ports"
+   // 27017 : mongoDB
+//    3000/3001 : safe port no.
