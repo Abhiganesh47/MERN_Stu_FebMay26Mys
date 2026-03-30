d036c50f1b69202d2c8c6ab62621b3b82837b3e8
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-30 11:14:13 +0530

    Handeling different HTTP methods in express

---
+++ W7/D1/P3.js
@@ -0,0 +1,21 @@
+// Handeling different HTTP methods in express
+const express = require("express");
+
+const app = express();
+
+// to read
+app.get("/users",function(req,res){
+res.send("Returning all users");  
+});
+
+app.post("/users",function(req,res){
+    // res.status() sets the HTTP status code before sending the 
+    res.status(201).send("User created.");
+});
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
+
+
+// running:curl -X POST http://localhost:4000/users
