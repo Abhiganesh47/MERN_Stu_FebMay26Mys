d39bebd00d8adbf103166a2cc7616622c595adac
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-30 13:07:26 +0530

    Global Middleware runs for every request.

---
+++ W7/D1/P5.js
@@ -0,0 +1,27 @@
+// 
+
+const express = require("express");
+
+const app = express();
+
+// Global Middleware runs for every request.
+app.use(function(req,res,next){
+    console.log("Globel Middleware",req.method,req.url);
+    
+    next();
+});
+
+app.get("/",function(req,res){
+res.send("Home route..");  
+});
+
+app.get("/admin",function(req,res,next){
+    console.log("Route specific middleware for /admin");
+    next();
+},function(req,res){
+    res.send("Admin route reached.");
+});
+
+app.listen(4000,function(){
+    console.log("Express server running at http://localhost:4000");
+});
