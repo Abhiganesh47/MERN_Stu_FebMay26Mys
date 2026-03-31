a09ed484f9975e048d615c855f341fbe3de8237d
Abhiganesh47 <abhiganesh47@gmail.com>
2026-03-31 17:12:40 +0530

    Added New Router

--- W7/D2/P1.js
+++ W7/D2/P1.js
@@ -26,6 +26,34 @@ apiRouter.get("/orders",function(req,res){
 // Mounting the route under the /api base path
 app.use("/api",apiRouter);
 
+// ****New Router****
+// product routers   /api/products
+const productRouter = express.Router();
+
+// /api/products      ->create product
+productRouter.post("/",(req,res)=>{
+    res.json({
+        route:"/api/products",
+        message:"Create Products"
+    });
+});
+
+// /api/products/:id     ->delete Products
+productRouter.delete("/:id", (req,res) => {
+    res.json({
+        route: `/api/products/${req.params.id}`,
+        message:"Delete product"
+    });
+});
+
+// mount products router under /api/products
+apiRouter.use("/products",productRouter);
+
 app.listen(4001,function(){
     console.log("Express server running at http://localhost:4001");
 });
+
+
+
+// curl -X POST http://localhost:4001/api/products -H "Content-Type:application/json" -d"{\"name\":\"Abhi\",\"role\":\"admin\"}"
+// curl -X DELETE http://localhost:4001/api/products/134  
