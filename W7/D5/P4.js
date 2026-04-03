7ff19e9df2d4f4f47e7af99a72e57ac423f80a2f
Abhiganesh47 <abhiganesh47@gmail.com>
2026-04-03 12:25:22 +0530

    Basics Of RBAC(role-based access control)

---
+++ W7/D5/P4.js
@@ -0,0 +1,15 @@
+// Basics Of RBAC(role-based access control)
+const routePermissions = {
+    "/admin":["admin"],
+    "/reports":["admin","manager"],
+    "/profile":["admin","manager","user"]
+};
+
+function canAccess(route,role){
+    const allowedRoles = routePermissions[route] || [];
+    return allowedRoles.includes(role);
+}
+console.log("User access to '/admin:",canAccess("/admin","user"));
+console.log("Admin access to '/admin:",canAccess("/admin","admin"));
+console.log("Manager  access to '/reports:",canAccess("/reports","manager"));
+console.log("User access to '/profile:",canAccess("/profile","user"));
