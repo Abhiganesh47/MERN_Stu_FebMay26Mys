8b95500d2c8faef2065f9ad4a46b20c2328c53cb
Abhiganesh47 <abhiganesh47@gmail.com>
2026-04-07 11:06:18 +0530

    modifyed and added functions

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V2.0/booking.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V2.0/booking.js
@@ -1,6 +1,6 @@
 // Handles booking related operation 
 const bookingEmitter = require("./events");
-
+const {appendBookingAsync,appendLogAsync} = require("./fileManager");
 let currentBooking = null;
 
 function getCurrentBooking(){
@@ -74,6 +74,7 @@ function processBooking(movie,showtime,seatCount){
     })
     .then(()=> generateBookingDetails(movie,showtime,seatCount))
     .then((booking)=>confirmBooking(booking,showtime))
+    .then((confirmedBooking)=>saveBookingToFile(confirmedBooking))
     .catch((error)=>{
         bookingEmitter.emit("bookingfailed",error);
         throw error;
@@ -95,6 +96,8 @@ async function processBookingAsync(movie,showtime,seatCount){
         
         const confirmedBooking = await confirmBooking(booking,showtime);
 
+        await saveBookingToFile(confirmedBooking);
+
         return confirmedBooking;
     }
     catch(error){
@@ -102,6 +105,16 @@ async function processBookingAsync(movie,showtime,seatCount){
         throw error;
     }
 }
+
+async function saveBookingToFile(booking) {
+    await appendBookingAsync(booking);
+    await appendLogAsync(`Booking saved: ${booking.bookingId} for ${booking.movieTitle}`);
+
+    bookingEmitter.emit("bookingSaved.",booking);
+    return booking;
+}
+
+
 
 module.exports = {
     getCurrentBooking,
