83bbda7813c65673242d0156b45a4d90b86f012f
Abhiganesh47 <abhiganesh47@gmail.com>
2026-04-09 20:21:21 +0530

    connecting to MongoDB using Mongoose and creating a schema and model

---
+++ W8/D1/P1.js
@@ -0,0 +1,28 @@
+// connecting to MongoDB using Mongoose and creating a schema and model
+const mongoose = require("mongoose");
+
+async function main(){
+    try{
+        await mongoose.connect("mongodb://localhost:27017/abcmern");
+        console.log("MongoDb connected successfully");
+
+        const userSchema = new mongoose.Schema({
+            name:String,
+            age:Number,
+            role:String
+        });
+
+        const User = mongoose.model("User",userSchema);
+
+        console.log("mongoose schems model created successfully");
+        console.log("Model name: ",User.modelName);
+
+        await mongoose.connection.close();
+        console.log("connection closed");
+    }
+    catch(error){
+        console.log("Failed to connect to DB",error.message);
+    }
+}
+
+main();
