bdf7b1285ad3b9972bd3f3ef49b97b90ef52d5a2
Abhiganesh47 <abhiganesh47@gmail.com>
2026-04-16 12:24:54 +0530

    otp generation and verification using crypto module of Node.js

---
+++ W8/D4/P5.js
@@ -0,0 +1,33 @@
+// otp generation and verification using crypto module of Node.js
+const crypto = require('crypto');
+
+const otpStore = {};
+
+function generateOTP(userId){
+    const otp =  crypto.randomInt(100000,999999)
+    .toString();
+    otpStore[userId]={
+        otp,
+        expiresAt:Date.now()+20000,
+        attempts:0
+    };
+    console.log("Otp: ",otp);
+}
+function verifyOTP(userId,enteredOtp){
+    const record = otpStore[userId];
+
+    if(!record){
+        return "No OTP";
+    }
+    if(Date.now()>record.expiresAt)return "Expired";
+    if(record.attempts>=3) return "Blocked";
+
+    record.attempts++;
+    return record.otp === String(enteredOtp)?"Valid": "Invalid";
+}
+generateOTP("user1");
+
+const userEnteredOtp = otpStore["user1"].otp;
+setTimeout(() => {
+console.log(verifyOTP("user1",userEnteredOtp));
+},10000);
