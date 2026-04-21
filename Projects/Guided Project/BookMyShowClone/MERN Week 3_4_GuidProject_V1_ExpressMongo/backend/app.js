6cec3f06f85ba806734b722c388ad9df0c39a464
Abhiganesh47 <abhiganesh47@gmail.com>
2026-04-21 15:00:53 +0530

    added auth routes

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const authRoutes = require("./src/routes/auth.routes");
+const { applyTimestamps } = require("./src/models/Show");
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth",authRoutes);
 
 app.get("/", (req, res) => {
     res.status(200).json({
