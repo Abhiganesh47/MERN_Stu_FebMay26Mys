a2b5db7a2eb92ea6013db74717013b0686d6ebd1
Abhiganesh47 <abhiganesh47@gmail.com>
2026-04-21 12:54:40 +0530

    Created otp schema , added TTL index , created and exported otp  model

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/OTP.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const otpSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        index:true
+    },
+    otp:{
+        type:String,
+        required:true,
+        select:false
+    },
+    expiresAt:{
+        type:Date,
+        required:true
+    },
+    attempts:{
+        type:Number,
+        default:0
+    }
+},
+    {
+        timestamps:true
+    }
+);
+
+//TTL index
+otpSchema.index({expiresAt:1},{expireAfterSeconds:0}); //keeping track of the otp
+
+//Export
+module.exports = mongoose.model("OTP",otpSchema);
