970123299cd1c9ebae82257564dcbe7d84a79b3b
Abhiganesh47 <abhiganesh47@gmail.com>
2026-04-23 12:22:55 +0530

    typo

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Show.js
@@ -54,12 +54,19 @@ const showSchema = new mongoose.Schema({
 //Compound index
 showSchema.index({movieId:1,date:1});
 
-//Add Validation
-showSchema.pre("save",function(next){
-    if(this.availableSeats>this.totalSeats){
-        return next(new Error("Available seats cannot exceed total seats"));
+// //Add Validation
+// showSchema.pre("save",function(next){
+//     if(this.availableSeats>this.totalSeats){
+//         return next(new Error("Available seats cannot exceed total seats"));
+//     }
+//     next();
+// });
+
+// New Add Validation code
+showSchema.pre("save", async function () {
+    if (this.availableSeats > this.totalSeats) {
+        throw new Error("Available seats cannot exceed total seats");
     }
-    next();
 });
 
 module.exports = mongoose.model("Show",showSchema)
