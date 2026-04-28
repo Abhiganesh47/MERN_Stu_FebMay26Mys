dc48c62b9d6b5ca5132ac9ac4a7bf93ee8b60c10
Abhiganesh47 <abhiganesh47@gmail.com>
2026-04-28 12:34:59 +0530

    React application to learn Component consepts

---
+++ W9/D2/my-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { FunctionName } from './components/FunctionalCompOne'
+import { FunctionalComponentsBasics } from './components/FunctionalComponentsBasics'
+import { ClassComponentsBasics } from './components/ClassComponentsBasics'
+import { FunctionComp } from './components/FunctionalComponentsAdv'
+function App() {
+  return (
+    // fragment: used to group multiple elements without adding extra nodes to the DOM :<> </> acts like a parent tags.
+    <>
+     {/* <FunctionName/> Component name */}
+     {/* <FunctionalComponentsBasics/> */}
+
+     {/* <ClassComponentsBasics/> */}
+
+     <FunctionComp/>
+    </>
+  )
+}
+export default App
