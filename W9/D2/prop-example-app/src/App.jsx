d7fbbc820bbadbcbac9e5c6bc06bded8060a2435
Abhiganesh47 <abhiganesh47@gmail.com>
2026-04-29 11:43:40 +0530

    SharedStateParent component for state lifting demonstration

--- W9/D2/prop-example-app/src/App.jsx
+++ W9/D2/prop-example-app/src/App.jsx
@@ -3,13 +3,15 @@ import { PropBasics } from './components/P1'
 import { PropDestructuring } from './components/P2'
 import { PropsChildren } from './components/P3';
 import {PropDrillingDemo} from './components/P4.jsx'
+import { SharedStateParent } from './components/P5.jsx';
 function App() {
   return (
     <>
       {/* <PropBasics/> */}
       {/* <PropDestructuring/> */}
       {/* <PropsChildren/> */}
-      <PropDrillingDemo/>
+      {/* <PropDrillingDemo/> */}
+      <SharedStateParent/>
     </>
   );
 }
