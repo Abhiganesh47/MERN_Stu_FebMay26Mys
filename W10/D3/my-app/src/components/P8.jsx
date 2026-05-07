132c80efbb9fca3f06c81d8c4c151bf67877652d
Abhiganesh47 <abhiganesh47@gmail.com>
2026-05-07 15:09:51 +0530

    KeyboardEvent component to demonstrate handling keyboard events in React

---
+++ W10/D3/my-app/src/components/P8.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+// Keyboard events in react
+export function KeyboardEvent(){
+    const [text, setText] = useState('');
+    const [action, setAction] = useState('No action yet');
+
+    // input handler
+    const handleChange = (event)=>{
+        setText(event.target.value);
+    };
+
+    // Keyboard event handler
+    const handleKeyDown = (event)=>{
+        if(event.key === 'Enter'){
+            setAction(`Submitted: ${text}`);
+        }
+        if(event.key === 'Escape'){
+            setText('');
+            setAction('Input cleared');
+        }
+    };
+
+    return(
+        <section>
+            <h2>Keyboard events</h2>
+            <label htmlFor="nameInput">Type something: </label>
+            <input type="text" id="nameInput" value={text} onChange={handleChange} onKeyDown={handleKeyDown}
+            placeholder="Press enter or ESC" />
+            <p>Current Input: {text}</p>
+            <p>Action : {action}</p>
+            <button onClick={()=>setAction('Button clicked')}>
+                Native Submit button
+            </button>
+        </section>
+    )
+}
