a6d0f1fbe02c93e74460a89dbb311d820dfb0320
Abhiganesh47 <abhiganesh47@gmail.com>
2026-06-12 14:05:32 +0530

    Add BookButton and GenreFilter components; update App to include them

--- W12/Final Frontend Project/src/App.jsx
+++ W12/Final Frontend Project/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 // import HomePage from './pages/HomePage'
 // import MovieCard from './components/MovieCard'
-import CitySelector from './components/CitySelector';
-import MovieSearch from './components/MovieSearch';
+// import CitySelector from './components/CitySelector';
+// import MovieSearch from './components/MovieSearch';
+import BookButton from './components/BookButton';
+import GenreFilter from './components/GenreFilter';
 function App() {
   return (
     <>
-    <CitySelector/>
+    {/* <CitySelector/> */}
     {/* Controlled Component */}
-    <MovieSearch/>
+    {/* <MovieSearch/> */}
+    <BookButton/>
+    <GenreFilter/>
      </>
   );
 }
